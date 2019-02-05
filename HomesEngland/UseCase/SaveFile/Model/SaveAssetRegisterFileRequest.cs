435d8c7861190ae411878d25c418ab2505d1b7fa
Jeff Pinkham <jeff@pinkhamsoftware.com>
2019-02-05 12:44:49 +0000

    remove file remnants from previous implementation

--- HomesEngland/UseCase/SaveFile/Model/SaveAssetRegisterFileRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomesEngland.UseCase.SaveFile.Model
-{
-    public class SaveAssetRegisterFileRequest
-    {
-        public string Text { get; set; }
-        public string FileName { get; set; }
-    }
-}
